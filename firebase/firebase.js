0b2072d041738b2f650ba140e985f9d902c2001e
Luis Infanzón <web@coleccionbelair.com>
2020-07-22 08:54:08 -0500

    carga de imagenes

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-// import 'firebase/storage';
+import 'firebase/storage';
 
 import firebaseConfig from './config';
 
@@ -12,7 +12,7 @@ class Firebase {
         }
         this.auth = app.auth();
         this.db = app.firestore();
-        // this.storage = app.storage();
+        this.storage = app.storage();
     }
 
     // Registra un usuario
--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,11 +1,13 @@
-import React, {useState, useContext} from 'react'
-import Router, {useRouter} from 'next/router';
-import FileUploader from 'react-firebase-file-uploader'; 
-import {css} from '@emotion/core';
+import React, { useState, useContext } from 'react';
+import { css } from '@emotion/core';
+import Router, { useRouter } from 'next/router';
+import FileUploader from 'react-firebase-file-uploader';
 import Layout from '../components/layout/Layout';
-import {Formulario, Campo, InputSubmit, Error} from '../components/ui/Formulario';
+import { Formulario, Campo, InputSubmit, Error } from '../components/ui/Formulario';
 
-import {FirebaseContext} from  '../firebase';
+import { FirebaseContext } from '../firebase';
+
+
 
 // validaciones
 import useValidacion from '../hooks/useValidacion';
@@ -13,52 +15,91 @@ import validarCrearProducto from '../validacion/validarCrearProducto';
 
 const STATE_INICIAL = {
   nombre: '',
-  empresa: '',
+  empresa: '', 
+  imagen: '',
   url: '',
   descripcion: ''
 }
 
 const NuevoProducto = () => {
 
-  const [error, guardarError] = useState(false);
+  // state de las imagenes
+  const [nombreimagen, guardarNombre] = useState('');
+  const [subiendo, guardarSubiendo] = useState(false);
+  const [ progreso, guardarProgreso ] = useState(0);
+  const [urlimagen, guardarUrlImagen] = useState('');
 
-  const {valores, errores, handleSubmit, handleChange, handleBlur} = useValidacion(STATE_INICIAL, validarCrearProducto, crearProducto );
+  const [ error, guardarError] = useState(false);
 
-  const { nombre, empresa, imagen, url, descripcion } = valores; // extraigo los "valores" de valores
+  const { valores, errores, handleSubmit, handleChange, handleBlur } = useValidacion(STATE_INICIAL, validarCrearProducto, crearProducto);
+
+  const { nombre, empresa, imagen, url, descripcion } = valores;
 
   // hook de routing para redireccionar
   const router = useRouter();
 
-  // context con las operaciones crud de firebase, funcionalidad de Firebase para crear los productos
-  const {usuario, firebase} = useContext(FirebaseContext);
-
-
+  // context con las operaciones crud de firebase
+  const { usuario, firebase } = useContext(FirebaseContext);
 
   async function crearProducto() {
-      
-// si el usuario no esta autenticado llevar al login
-if(!usuario) {
-  return router.push('/login');
-}
 
-// crear el objeto de nuevo producto
-const producto = {
-  nombre,
-  empresa,
-  url,
-  descripcion,
-  votos: 0,
-  comentarios: [],
-  creado: Date.now()
-}
+    // si el usuario no esta autenticado llevar al login
+    if(!usuario) {
+      return router.push('/login');
+    }
 
-// insertarlo en la base de datos
-firebase.db.collection('productos').add(producto);
+    // crear el objeto de nuevo producto 
+    const producto = {
+        nombre, 
+        empresa, 
+        url, 
+        urlimagen,
+        urlimagen,
+        descripcion,
+        votos: 0,
+        comentarios: [],
+        creado: Date.now(), 
+        creador: {
+          id: usuario.uid,
+          nombre: usuario.displayName
+        }, 
+        haVotado: []
+    }
 
+    // insertarlo en la base de datos
+    firebase.db.collection('productos').add(producto);
 
-
+    return router.push('/');
 
   }
+
+
+  const handleUploadStart = () => {
+      guardarProgreso(0);
+      guardarSubiendo(true);
+  }
+
+  const handleProgress = progreso => guardarProgreso({ progreso });
+
+  const handleUploadError = error => {
+      guardarSubiendo(error);
+      console.error(error);
+  };
+
+  const handleUploadSuccess = nombre => {
+      guardarProgreso(100);
+      guardarSubiendo(false);
+      guardarNombre(nombre)
+      firebase
+          .storage
+          .ref("productos")
+          .child(nombre)
+          .getDownloadURL()
+          .then(url => {
+            console.log(url);
+            guardarUrlImagen(url);
+          } );
+  };
 
   return (
     <div>
@@ -108,10 +149,7 @@ firebase.db.collection('productos').add(producto);
                 <FileUploader 
                   accept="image/*"
                   id="imagen" 
-                  name="imagen"
-                  value={imagen}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
+                  name="imagen" 
                   randomizeFilename
                   storageRef={firebase.storage.ref("productos")}
                   onUploadStart = {handleUploadStart}
@@ -120,7 +158,7 @@ firebase.db.collection('productos').add(producto);
                   onProgress={handleProgress}
                   />
               </Campo>
-              {errores.imagen && <Error>{errores.imagen}</Error>}
+             
 
               <Campo>
                 <label htmlFor="url">URL</label>
